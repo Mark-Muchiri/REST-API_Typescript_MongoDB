1ed722c6d521f8fafc7695e56fcc29bc8dea540f
Mark <muchirimark2@gmail.com>
2023-07-23 23:40:58 +0300

    - was able to finish the login feature - I was also able to fetch the total users only if the ligin is authorised

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,33 @@
-import express from 'express';
-import {get, merge} from 'lodash'
+import express from "express";
+import { get, merge } from "lodash";
+import { getUserBySessionToken } from "../controller/UserController";
+
+export const isAuthenticated = async (
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction
+) => {
+	try {
+		const sessionToken = req.cookies["MARK-AUTH"];
+
+		// if token expires
+		if (!sessionToken) {
+			return res.sendStatus(403).json({ message: "please login again" });
+		}
+
+		const existingUser = await getUserBySessionToken(sessionToken);
+
+		if (!existingUser) {
+			return res
+				.sendStatus(403)
+				.json({ message: "there's an existing user already" });
+		}
+
+		merge(req, { identity: existingUser });
+
+		return next();
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(403);
+	}
+};
